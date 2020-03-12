452e6a0a14ddc5244b24f85000abd690982f65e7
Chris Wall <chriswall@dhcp-168-105-223-195.wireless.manoa.hawaii.edu>
2020-03-11 14:48:20 -1000

    Ndfa

--- scripts/ndfa script.R
+++ scripts/ndfa script.R
@@ -140,33 +140,252 @@ abline(mod.RKoa, col="darkcyan", lty=3)
 
 ################# Need new dataframe of KOA and Rubus 
 ########### separate data frame
+# full Hak data dataframe
+Ndfa.Hak<- Hak.data %>%
+  select(c(Plot, Sample, d15N))
 
-Ndfa.AK<-Hak.data[(Hak.data$Plot=="AK"),]
-Ndfa.RK<-Hak.data[(Hak.data$Plot=="RK"),]
+Ndfa.AK<-Ndfa.Hak[(Ndfa.Hak$Plot=="AK"),]
+Ndfa.RK<-Ndfa.Hak[(Ndfa.Hak$Plot=="RK"),]
 
 # AK Rubs
 AK.R.min<-min(Ndfa.AK[(Ndfa.AK$Sample=="RUBHAW"),]$d15N) # 0.6
 AK.R.max<-max(Ndfa.AK[(Ndfa.AK$Sample=="RUBHAW"),]$d15N) # 3.15
 AK.R.med<-median(Ndfa.AK[(Ndfa.AK$Sample=="RUBHAW"),]$d15N) # 1.94
+AK.R.av<-mean(Ndfa.AK[(Ndfa.AK$Sample=="RUBHAW"),]$d15N) # 1.9
 
 # RK Rubs
 RK.R.min<-min(Ndfa.RK[(Ndfa.RK$Sample=="RUBARG"),]$d15N) # 0.99
 RK.R.max<-max(Ndfa.RK[(Ndfa.RK$Sample=="RUBARG"),]$d15N) # 4.53
 RK.R.med<-median(Ndfa.RK[(Ndfa.RK$Sample=="RUBARG"),]$d15N) # 3.03
+RK.R.av<-mean(Ndfa.RK[(Ndfa.RK$Sample=="RUBARG"),]$d15N) # 2.93
 
 # AK and RK Koa dataframes
 AK.Koa.df<-Ndfa.AK[(Ndfa.AK$Sample=="Koa"),]
 RK.Koa.df<-Ndfa.AK[(Ndfa.RK$Sample=="Koa"),]
 
+
+##### Beta -1 plot
+# Beta negative 1
 # AK koa Ndfa
-AK.Koa.df<- AK.Koa.df %>%
-  mutate(nfix.AK.min = (AK.R.min - d15N)/(AK.R.min-Beta0.5)*100) %>%
-  mutate(nfix.AK.max = (AK.R.min - d15N)/(AK.R.max-Beta0.5)*100)  %>%
-  mutate(nfix.AK.med = (AK.R.min - d15N)/(AK.R.med-Beta0.5)*100)
+AK.Koa.df.betneg1<- AK.Koa.df %>%
+  mutate(nfix.AK.min = (AK.R.min - d15N)/(AK.R.min-Beta.neg1)*100) %>%
+  mutate(nfix.AK.max = (AK.R.max - d15N)/(AK.R.max-Beta.neg1)*100)  %>%
+  mutate(nfix.AK.med = (AK.R.med - d15N)/(AK.R.med-Beta.neg1)*100) %>%
+  mutate(nfix.AK.av = (AK.R.av - d15N)/(AK.R.av-Beta.neg1)*100)
 
 # RK koa Ndfa
-RK.Koa.df<- RK.Koa.df %>%
-  mutate(nfix.RK.min = (RK.R.min - d15N)/(RK.R.min-Beta0.5)*100) %>%
-  mutate(nfix.RK.max = (RK.R.min - d15N)/(RK.R.max-Beta0.5)*100)  %>%
-  mutate(nfix.RK.med = (RK.R.min - d15N)/(RK.R.med-Beta0.5)*100)
+RK.Koa.df.betneg1<- RK.Koa.df %>%
+  mutate(nfix.RK.min = (RK.R.min - d15N)/(RK.R.min-Beta.neg1)*100) %>%
+  mutate(nfix.RK.max = (RK.R.max - d15N)/(RK.R.max-Beta.neg1)*100)  %>%
+  mutate(nfix.RK.med = (RK.R.med - d15N)/(RK.R.med-Beta.neg1)*100) %>%
+  mutate(nfix.RK.av = (RK.R.av - d15N)/(RK.R.av-Beta.neg1)*100)
 
+
+## Max
+par(mfrow=c(1,2), mar=c(5,5,4,2))
+
+# build empty plot
+plot(data=AK.Koa.df.betneg1, d15N~nfix.AK.max, yaxt="n", xaxt="n", ylim=c(-2,3), xlim=c(100, -30), 
+     xlab="", ylab="", type="n")
+
+# add shaded areas
+# max and median are highest and lowest respectively
+
+# models (Max)
+B1.mod.AKoa.max<-lm(d15N~nfix.AK.max, data=AK.Koa.df.betneg1) 
+B1.mod.RKoa.max<-lm(d15N~nfix.RK.max, data=RK.Koa.df.betneg1)
+
+# models (Median)
+B1.mod.AKoa.med<-lm(d15N~nfix.AK.med, data=AK.Koa.df.betneg1)
+B1.mod.RKoa.med<-lm(d15N~nfix.RK.med, data=RK.Koa.df.betneg1)
+
+par(new=T)
+
+#for AK
+x <- seq(-40,100, 1) # sequence
+ymax.AK <- x*(B1.mod.AKoa.max$coefficients[2])+B1.mod.AKoa.max$coefficients[1] # x*slope + intercept
+ymed.AK <- x*(B1.mod.AKoa.med$coefficients[2])+B1.mod.AKoa.med$coefficients[1]
+polygon(c(rev(x), x),c(rev(ymax.AK), ymed.AK),col=adjustcolor("salmon",alpha.f=0.2), border=NA)
+
+#for RK
+ymax.RK <- x*(B1.mod.RKoa.max$coefficients[2])+B1.mod.RKoa.max$coefficients[1] # x*slope + intercept
+ymed.RK <- x*(B1.mod.RKoa.med$coefficients[2])+B1.mod.RKoa.med$coefficients[1]
+polygon(c(rev(x), x),c(rev(ymax.RK), ymed.RK),col=adjustcolor("lightseagreen",alpha.f=0.2), border=NA)
+
+# plot points and model fits
+par(new=T)
+
+# AK 
+plot(data=AK.Koa.df.betneg1, d15N~nfix.AK.max, col="salmon", xlab="Ndf-koa (%)",
+     ylab= expression(paste(delta^{15}, N, " (\u2030)")), cex.axis=0.7, main= "Beta = -1", cex.main=0.7,
+     ylim=c(-2,3), xlim=c(100, -30), cex=0.7, pch=20)
+
+#RK
+par(new=T)
+plot(data=RK.Koa.df.betneg1, d15N~nfix.RK.max, col="lightseagreen", xlab="", ylab= "", xaxt="n", yaxt="n",
+     ylim=c(-2,3), xlim=c(100, -30), cex=0.7, pch=20)
+legend("topleft", legend= c("AK", "RK"), bg="transparent", 
+       col=c("salmon", "lightseagreen"), 
+       pch=20, box.lty=0)
+
+# add model lines
+abline(B1.mod.AKoa.max, col="coral", lty=1)
+abline(B1.mod.RKoa.max, col="darkcyan", lty=1)
+
+## Mean
+# AK 
+par(new=T)
+plot(data=AK.Koa.df.betneg1, d15N~nfix.AK.av, col="salmon", xlab="", ylab= "", xaxt="n", yaxt="n",
+     ylim=c(-2,3), xlim=c(100, -30), cex=0.7, pch=20)
+
+#RK
+par(new=T)
+plot(data=RK.Koa.df.betneg1, d15N~nfix.RK.av, col="lightseagreen", xlab="", ylab= "", xaxt="n", yaxt="n",
+     ylim=c(-2,3), xlim=c(100, -30), cex=0.7, pch=20)
+
+# model
+B1.mod.AKoa.av<-lm(d15N~nfix.AK.av, data=AK.Koa.df.betneg1)
+B1.mod.RKoa.av<-lm(d15N~nfix.RK.av, data=RK.Koa.df.betneg1)
+
+# add model lines
+abline(B1.mod.AKoa.av, col="coral", lty=2)
+abline(B1.mod.RKoa.av, col="darkcyan", lty=2)
+
+
+## Median
+# AK 
+par(new=T)
+plot(data=AK.Koa.df.betneg1, d15N~nfix.AK.med, col="salmon", xlab="", ylab= "", xaxt="n", yaxt="n",
+     ylim=c(-2,3), xlim=c(100, -30), cex=0.7, pch=20)
+
+#RK
+par(new=T)
+plot(data=RK.Koa.df.betneg1, d15N~nfix.RK.med, col="lightseagreen", xlab="", ylab= "", xaxt="n", yaxt="n",
+     ylim=c(-2,3), xlim=c(100, -30), cex=0.7, pch=20)
+legend("bottomright", legend= c("max", "mean", "median"), bg="transparent",
+       col="black", lty=c(1,2,4), 
+       pch=20, cex=0.7, box.lty=0)
+
+# model
+B1.mod.AKoa.med<-lm(d15N~nfix.AK.med, data=AK.Koa.df.betneg1)
+B1.mod.RKoa.med<-lm(d15N~nfix.RK.med, data=RK.Koa.df.betneg1)
+
+# add model lines
+abline(B1.mod.AKoa.med, col="coral", lty=4)
+abline(B1.mod.RKoa.med, col="darkcyan", lty=4)
+
+###################
+###################
+# Beta negative 2
+
+##### Beta -2 plot
+# Beta negative 2
+# AK koa Ndfa
+AK.Koa.df.betneg2<- AK.Koa.df %>%
+  mutate(nfix.AK.min = (AK.R.min - d15N)/(AK.R.min-Beta.neg2)*100) %>%
+  mutate(nfix.AK.max = (AK.R.max - d15N)/(AK.R.max-Beta.neg2)*100)  %>%
+  mutate(nfix.AK.med = (AK.R.med - d15N)/(AK.R.med-Beta.neg2)*100) %>%
+  mutate(nfix.AK.av = (AK.R.av - d15N)/(AK.R.av-Beta.neg2)*100)
+
+# RK koa Ndfa
+RK.Koa.df.betneg2<- RK.Koa.df %>%
+  mutate(nfix.RK.min = (RK.R.min - d15N)/(RK.R.min-Beta.neg2)*100) %>%
+  mutate(nfix.RK.max = (RK.R.max - d15N)/(RK.R.max-Beta.neg2)*100)  %>%
+  mutate(nfix.RK.med = (RK.R.med - d15N)/(RK.R.med-Beta.neg2)*100) %>%
+  mutate(nfix.RK.av = (RK.R.av - d15N)/(RK.R.av-Beta.neg2)*100)
+
+
+## Max
+
+# build empty plot
+plot(data=AK.Koa.df.betneg2, d15N~nfix.AK.max, yaxt="n", xaxt="n", ylim=c(-2,3), xlim=c(100, -30), 
+     xlab="", ylab="", type="n")
+
+# add shaded areas
+# max and median are highest and lowest respectively
+
+# models (Max)
+B2.mod.AKoa.max<-lm(d15N~nfix.AK.max, data=AK.Koa.df.betneg2) 
+B2.mod.RKoa.max<-lm(d15N~nfix.RK.max, data=RK.Koa.df.betneg2)
+
+# models (Median)
+B2.mod.AKoa.med<-lm(d15N~nfix.AK.med, data=AK.Koa.df.betneg2)
+B2.mod.RKoa.med<-lm(d15N~nfix.RK.med, data=RK.Koa.df.betneg2)
+
+par(new=T)
+
+#for AK
+x <- seq(-40,100, 1) # sequence
+ymax.AK <- x*(B2.mod.AKoa.max$coefficients[2])+B2.mod.AKoa.max$coefficients[1] # x*slope + intercept
+ymed.AK <- x*(B2.mod.AKoa.med$coefficients[2])+B2.mod.AKoa.med$coefficients[1]
+polygon(c(rev(x), x),c(rev(ymax.AK), ymed.AK),col=adjustcolor("salmon",alpha.f=0.2), border=NA)
+
+#for RK
+ymax.RK <- x*(B2.mod.RKoa.max$coefficients[2])+B2.mod.RKoa.max$coefficients[1] # x*slope + intercept
+ymed.RK <- x*(B2.mod.RKoa.med$coefficients[2])+B2.mod.RKoa.med$coefficients[1]
+polygon(c(rev(x), x),c(rev(ymax.RK), ymed.RK),col=adjustcolor("lightseagreen",alpha.f=0.2), border=NA)
+
+# plot points and model fits
+par(new=T)
+
+# AK 
+plot(data=AK.Koa.df.betneg2, d15N~nfix.AK.max, col="salmon", xlab="Ndf-koa (%)",
+     ylab= expression(paste(delta^{15}, N, " (\u2030)")), cex.axis=0.7, main= "Beta = -2", cex.main=0.7,
+     ylim=c(-2,3), xlim=c(100, -30), cex=0.7, pch=20)
+
+#RK
+par(new=T)
+plot(data=RK.Koa.df.betneg2, d15N~nfix.RK.max, col="lightseagreen", xlab="", ylab= "", xaxt="n", yaxt="n",
+     ylim=c(-2,3), xlim=c(100, -30), cex=0.7, pch=20)
+legend("topleft", legend= c("AK", "RK"), bg="transparent", 
+       col=c("salmon", "lightseagreen"), 
+       pch=20, box.lty=0)
+
+# add model lines
+abline(B2.mod.AKoa.max, col="coral", lty=1)
+abline(B2.mod.RKoa.max, col="darkcyan", lty=1)
+
+## Mean
+# AK 
+par(new=T)
+plot(data=AK.Koa.df.betneg2, d15N~nfix.AK.av, col="salmon", xlab="", ylab= "", xaxt="n", yaxt="n",
+     ylim=c(-2,3), xlim=c(100, -30), cex=0.7, pch=20)
+
+#RK
+par(new=T)
+plot(data=RK.Koa.df.betneg2, d15N~nfix.RK.av, col="lightseagreen", xlab="", ylab= "", xaxt="n", yaxt="n",
+     ylim=c(-2,3), xlim=c(100, -30), cex=0.7, pch=20)
+
+# model
+B2.mod.AKoa.av<-lm(d15N~nfix.AK.av, data=AK.Koa.df.betneg2)
+B2.mod.RKoa.av<-lm(d15N~nfix.RK.av, data=RK.Koa.df.betneg2)
+
+# add model lines
+abline(B2.mod.AKoa.av, col="coral", lty=2)
+abline(B2.mod.RKoa.av, col="darkcyan", lty=2)
+
+
+## Median
+# AK 
+par(new=T)
+plot(data=AK.Koa.df.betneg2, d15N~nfix.AK.med, col="salmon", xlab="", ylab= "", xaxt="n", yaxt="n",
+     ylim=c(-2,3), xlim=c(100, -30), cex=0.7, pch=20)
+
+#RK
+par(new=T)
+plot(data=RK.Koa.df.betneg2, d15N~nfix.RK.med, col="lightseagreen", xlab="", ylab= "", xaxt="n", yaxt="n",
+     ylim=c(-2,3), xlim=c(100, -30), cex=0.7, pch=20)
+legend("bottomright", legend= c("max", "mean", "median"), bg="transparent",
+       col="black", lty=c(1,2,4), 
+       pch=20, cex=0.7, box.lty=0)
+
+# model
+B2.mod.AKoa.med<-lm(d15N~nfix.AK.med, data=AK.Koa.df.betneg2)
+B2.mod.RKoa.med<-lm(d15N~nfix.RK.med, data=RK.Koa.df.betneg2)
+
+# add model lines
+abline(B2.mod.AKoa.med, col="coral", lty=4)
+abline(B2.mod.RKoa.med, col="darkcyan", lty=4)
+
+dev.copy(pdf, "figures/Ndfa.pdf", width=8, height=5, encod="MacRoman")
+dev.off()
